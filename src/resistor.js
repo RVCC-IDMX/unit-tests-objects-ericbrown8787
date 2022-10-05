348e1923a4e1547273e04df0fea262758731b848
Eric Brown <eric.brown8787@gmail.com>
2022-10-05 16:52:20 -0400

    Complete all problems
    All problems pass tests with 100% coverage

--- src/resistor.js
+++ src/resistor.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 /* resistor.js */
 
+// const { k } = require('vitest/dist/index-6e18a03a');
+
 /*
   Here is a illustration of a 4-banded electronic resistor:
     https://en.wikipedia.org/wiki/File:4-Band_Resistor.svg
@@ -62,7 +64,19 @@
  * then use the copied object like a lookup table
  */
 function getColorValue(color) {
-  // write your code here & return value
+  const colorCodes = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  };
+  return colorCodes[color];
 }
 
 /**
@@ -79,7 +93,22 @@ function getColorValue(color) {
  * then use the copied object like a lookup table
  */
 function getMultiplierValue(color) {
-  // write your code here & return value
+  const multiplierCodes = {
+    black: 1,
+    brown: 10,
+    red: 100,
+    orange: 1000,
+    yellow: 10000,
+    green: 100000,
+    blue: 1000000,
+    violet: 10000000,
+    grey: 100000000,
+    white: 1000000000,
+    gold: 0.1,
+    silver: 0.01,
+  };
+
+  return multiplierCodes[color];
 }
 
 /**
@@ -106,7 +135,16 @@ function getMultiplierValue(color) {
  *
  */
 function getThreeBandValue(bands) {
-  // write your code here & return value
+  const rawValue = +`${getColorValue(bands.color1)}${getColorValue(bands.color2)}` * getMultiplierValue(bands.multiplier);
+  let formattedValue;
+  if (bands.multiplier === 'gold') {
+    formattedValue = +rawValue.toFixed(1);
+  } else if (bands.multiplier === 'silver') {
+    formattedValue = +rawValue.toFixed(2);
+  } else {
+    formattedValue = rawValue;
+  }
+  return formattedValue;
 }
 
 /**
@@ -131,58 +169,87 @@ function getThreeBandValue(bands) {
  *
  */
 function formatNumber(val) {
-  // write your code here & return value
+  const suffixes = {
+    thousands: 'k',
+    millions: 'M',
+    billions: 'G',
+  };
+
+  if (val >= 1000000000) {
+    return +(val / 1000000000).toFixed(1) + suffixes.billions;
+  }
+  if (val >= 1000000) {
+    return +(val / 1000000).toFixed(1) + suffixes.millions;
+  }
+  if (val >= 1000) {
+    return +(val / 1000).toFixed(1) + suffixes.thousands;
+  }
+  return `${val}`;
 }
 
 /**
- * Returns the tolerance of the resistor according to its color value
- * @param {string} color - the color of the tolerance band to
- * @returns {string} - the tolerance value in percent using the ± symbol
- *
- * Go to http://bit.ly/2NjS274 see what colors are used for the tolerance
- * lookup table.
- *
- * must create a toleranceCodes object inside this function so it is private,
- * then use the object like a lookup table
- *
- * example: 'brown' => '±1%'
- * example: 'red' => '±2%'
- * example: 'green' => '±0.5%'
- */
+   * Returns the tolerance of the resistor according to its color value
+   * @param {string} color - the color of the tolerance band to
+   * @returns {string} - the tolerance value in percent using the ± symbol
+   *
+   * Go to http://bit.ly/2NjS274 see what colors are used for the tolerance
+   * lookup table.
+   *
+   * must create a toleranceCodes object inside this function so it is private,
+   * then use the object like a lookup table
+   *
+   * example: 'brown' => '±1%'
+   * example: 'red' => '±2%'
+   * example: 'green' => '±0.5%'
+   */
 function getTolerance(color) {
-  // write your code here & return value
+  const toleranceCodes = {
+    brown: '±1%',
+    red: '±2%',
+    green: '±0.5%',
+    blue: '±0.25%',
+    violet: '±0.1%',
+    grey: '±0.05%',
+    gold: '±5%',
+    silver: '±10%',
+  };
+
+  return toleranceCodes[color];
 }
 
 /**
- *
- * @param {object} bands - the object with the 4 bands
- * @param {string} bands.color1 - the first color
- * @param {string} bands.color2 - the second color
- * @param {string} bands.multiplier - the multiplier color
- * @param {string} bands.tolerance - the tolerance color
- * @returns {string} - a string representation of the resistor value according to
- * the format in the examples
- *
- * example: {
- *   color1: 'brown',
- *   color2: 'red',
- *   multiplier: 'black',
- *   tolerance: 'brown'
- * }
- *   => '12 Ohms ±1%'
- *
- * example: {
- *   color1: 'green',
- *   color2: 'blue',
- *   multiplier: 'red',
- *   tolerance: 'grey'
- * }
- *   => 'Resistor value: 5.6k Ohms ±0.05%'
- *
- * must use functions in this file to build the string using a template literal
- */
+   *
+   * @param {object} bands - the object with the 4 bands
+   * @param {string} bands.color1 - the first color
+   * @param {string} bands.color2 - the second color
+   * @param {string} bands.multiplier - the multiplier color
+   * @param {string} bands.tolerance - the tolerance color
+   * @returns {string} - a string representation of the resistor value according to
+   * the format in the examples
+   *
+   * example: {
+   *   color1: 'brown',
+   *   color2: 'red',
+   *   multiplier: 'black',
+   *   tolerance: 'brown'
+   * }
+   *   => '12 Ohms ±1%'
+   *
+   * example: {
+   *   color1: 'green',
+   *   color2: 'blue',
+   *   multiplier: 'red',
+   *   tolerance: 'grey'
+   * }
+   *   => 'Resistor value: 5.6k Ohms ±0.05%'
+   *
+   * must use functions in this file to build the string using a template literal
+   */
 function getResistorOhms(bands) {
-  // write your code here & return value
+  const rawValue = getThreeBandValue(bands);
+  const formattedValue = formatNumber(rawValue);
+  const tolerance = getTolerance(bands.tolerance);
+  return `Resistor value: ${formattedValue} Ohms ${tolerance}`;
 }
 
 module.exports = {
